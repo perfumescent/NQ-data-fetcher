d55870a407739311750ede98e828c4deffc4d4a8
ybw <fillory@qq.com>
2026-08-24 00:01:37 +0800

    120s 到 60s

--- src/main.py
+++ src/main.py
@@ -15,8 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.jobs.schedulers import RawJobScheduler
 from src.jobs.raw_push import push_raw_payload
 
-INTERVAL_SECONDS = 120
-
+INTERVAL_SECONDS = 60
 
 def main():
     """
